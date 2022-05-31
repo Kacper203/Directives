f150c19e40163a4936986a62cb198a74d523fce7
Kacper203 <kacper.kr.002@gmail.com>
2022-05-31 20:48:31 +0200

    add-space-between-numbers

--- Directives/src/app/app.module.ts
+++ Directives/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreditCardDirective } from './credit-card/credit-card.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardDirective
   ],
   imports: [
     BrowserModule,
---
+++ Directives/src/app/credit-card/credit-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'credit-card',
+    templateUrl: './credit-card.component.html',
+})
+
+export class AppComponent {
+}
